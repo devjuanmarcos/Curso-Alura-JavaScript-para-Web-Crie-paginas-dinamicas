8bb1eaede29ab5101eaf1b3b43193f62cdd7022f
Juan Marcos <jncontatojn@gmail.com>
2023-07-25 11:04:52 -0300

    repeticao otimizada com for

--- Curso-Alura-JavaScript-para-Web-Crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
+++ Curso-Alura-JavaScript-para-Web-Crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
@@ -6,19 +6,14 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
-//enquanto
-while (contador < listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 
-  // console.log(instrumento);
-
-  //template String
   const idAudio = `#som_${instrumento}`;
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-  contador = contador + 1;
-  console.log(contador);
 }
