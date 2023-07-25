21129d0553d5c235ffebe2c654e2841d621c9255
Juan Marcos <jncontatojn@gmail.com>
2023-07-25 10:46:14 -0300

    criacao da funcao anonima

--- Curso-Alura-JavaScript-para-Web-Crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
+++ Curso-Alura-JavaScript-para-Web-Crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -8,7 +8,9 @@ let contador = 0;
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSomPom;
+  listaDeTeclas[contador].onclick = function () {
+    tocaSom("#som_tecla_pom");
+  };
   contador = contador + 1;
   console.log(contador);
 }
